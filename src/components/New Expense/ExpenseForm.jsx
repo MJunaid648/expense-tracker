16292e4588be21e07ac29cd7208367aff752582e
Muhammad Junaid <648mjunaid@gmail.com>
2023-05-27 13:49:31 +0500

    completed tracker app

---
+++ src/components/New Expense/ExpenseForm.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState(0);
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const AmountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const DateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+            required
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            value={enteredAmount}
+            onChange={AmountChangeHandler}
+            min="0.01"
+            step="0.01"
+            required
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            value={enteredDate}
+            onChange={DateChangeHandler}
+            min="2022-01-01"
+            max="2025-12-31"
+            required
+          />
+        </div>
+      </div>
+      <div className="add-expense__action">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
---
+++ src/components/New Expense/NewExpense.jsx
@@ -0,0 +1,18 @@
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddNewExpense(expenseData);
+  };
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
